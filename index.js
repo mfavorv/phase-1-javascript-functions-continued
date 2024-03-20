89f16db69a1f05fda98f10b34bafc947063f7039
Favor Vincent <vfavorm77@gmail.com>
2024-03-20 09:13:45 +0300

    JS functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+      return `This Saturday, I want to ${activity}!`;
+   }
+
+   function mondayWork(job = "go to the office"){
+     return `This Monday, I will ${job}.`;
+   }
+
+   function wrapAdjective(wrap = "*"){
+    return function (adjective = "special"){
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+     
+   }
+   
